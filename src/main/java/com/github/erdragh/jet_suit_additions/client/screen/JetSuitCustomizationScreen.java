d573fb421f539be2df99c1e3feb1de27cd9b1bdb
Jan Bayer <enderjanyt@gmail.com>
2023-03-08 18:35:31 +0100

    remove all references of trying to build particles into the gui

--- src/main/java/com/github/erdragh/jet_suit_additions/client/screen/JetSuitCustomizationScreen.java
+++ src/main/java/com/github/erdragh/jet_suit_additions/client/screen/JetSuitCustomizationScreen.java
@@ -1,38 +1,19 @@
 package com.github.erdragh.jet_suit_additions.client.screen;
 
 import com.github.erdragh.jet_suit_additions.JetSuitAdditions;
-import com.github.erdragh.jet_suit_additions.client.GUIScene;
-import com.github.erdragh.jet_suit_additions.client.JetSuitAdditionsClient;
-import com.github.erdragh.jet_suit_additions.client.SuperRenderTypeBuffer;
 import com.github.erdragh.jet_suit_additions.particle.JetSuitParticles;
 import com.github.erdragh.jet_suit_additions.networking.C2SPackets;
-import com.google.common.collect.*;
-import com.mojang.blaze3d.platform.Lighting;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Matrix4f;
-import com.mojang.math.Quaternion;
-import com.mojang.math.Vector3f;
-import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
-import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.gui.screens.inventory.BookViewScreen;
-import net.minecraft.client.gui.screens.inventory.InventoryScreen;
-import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.levelgen.structure.BoundingBox;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
 
@@ -46,8 +27,6 @@ public class JetSuitCustomizationScreen extends Screen {
 
     private ArrayList<Button> buttons = new ArrayList<>();
 
-    private GUIScene scene;
-
 
     public JetSuitCustomizationScreen(Player player, ItemStack stack, InteractionHand hand) {
         super(new TranslatableComponent(JetSuitAdditions.MODID + ".gui.jet_suit_customization"));
@@ -55,8 +34,6 @@ public class JetSuitCustomizationScreen extends Screen {
         this.hand = hand;
 
         this.player = player;
-
-        scene = new GUIScene(player.getLevel(), player);
     }
 
     @Override
@@ -84,8 +61,6 @@ public class JetSuitCustomizationScreen extends Screen {
 
         RenderSystem.enableBlend();
 
-        renderScene(matrices, mouseX, mouseY, delta);
-
         var displayedText = new TranslatableComponent(JetSuitAdditions.MODID + ".gui.jet_suit_customization.title");
         var textWidth = this.font.width(displayedText);
         this.font.draw(matrices, displayedText, (this.width - textWidth) / 2, (this.height - WINDOW_HEIGHT) / 2 - 20, 0xffffff);
@@ -94,33 +69,6 @@ public class JetSuitCustomizationScreen extends Screen {
             button.render(matrices, mouseX, mouseY, delta);
         }
 
-        renderScene(matrices, mouseX, mouseY, delta);
-
-    }
-
-    public void renderScene(PoseStack matrices, int mouseX, int mouseY, float delta) {
-        SuperRenderTypeBuffer buffer = SuperRenderTypeBuffer.getInstance();
-
-        RenderSystem.enableBlend();
-        RenderSystem.enableDepthTest();
-        RenderSystem.backupProjectionMatrix();
-
-        Matrix4f matrix4f = new Matrix4f(RenderSystem.getProjectionMatrix());
-        matrix4f.multiplyWithTranslation(0,0,800);
-        RenderSystem.setProjectionMatrix(matrix4f);
-
-        matrices.pushPose();
-        matrices.translate(0,0,800);
-
-        scene.renderScene(buffer, matrices, delta);
-
-        buffer.draw();
-
-        matrices.pushPose();
-
-        matrices.popPose();
-        matrices.popPose();
-        RenderSystem.restoreProjectionMatrix();
     }
 
     @Override
@@ -131,6 +79,5 @@ public class JetSuitCustomizationScreen extends Screen {
     @Override
     public void tick() {
         super.tick();
-        this.scene.tick();
     }
 }
--- src/main/java/com/github/erdragh/jet_suit_additions/util/TStack.java
+++
@@ -1,7 +0,0 @@
-package com.github.erdragh.jet_suit_additions.util;
-
-public interface TStack<Self> {
-    Self pushPose();
-
-    Self popPose();
-}
