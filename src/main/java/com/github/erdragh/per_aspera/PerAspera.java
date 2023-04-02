0be3e5a00a27e4763e151feda4bb25fabd7ae36b
Jan Bayer <enderjanyt@gmail.com>
2023-04-02 17:11:52 +0200

    add initial mob AI

--- src/main/java/com/github/erdragh/per_aspera/PerAspera.java
+++ src/main/java/com/github/erdragh/per_aspera/PerAspera.java
@@ -14,6 +14,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.entity.animal.Cow;
 import net.minecraft.world.item.Item;
 import org.slf4j.Logger;
 
--- src/main/java/com/github/erdragh/per_aspera/entity/SlimeGirlEntity.java
+++ src/main/java/com/github/erdragh/per_aspera/entity/SlimeGirlEntity.java
@@ -1,14 +1,32 @@
 package com.github.erdragh.per_aspera.entity;
 
+import net.minecraft.client.model.CowModel;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.FloatGoal;
+import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+
+import java.util.Objects;
 
 public class SlimeGirlEntity extends PathfinderMob {
 
     public SlimeGirlEntity(EntityType<? extends PathfinderMob> entityType, Level level) {
         super (entityType, level);
+        Objects.requireNonNull(this.getAttribute(Attributes.MOVEMENT_SPEED)).setBaseValue(.2);
     }
 
+    @Override
+    protected void registerGoals() {
+        this.goalSelector.addGoal(0, new FloatGoal(this));
+        this.goalSelector.addGoal(1, new WaterAvoidingRandomStrollGoal(this, 1));
+        this.goalSelector.addGoal(2, new LookAtPlayerGoal(this, Player.class, 6.0f));
+        this.goalSelector.addGoal(3, new RandomLookAroundGoal(this));
+    }
 }
