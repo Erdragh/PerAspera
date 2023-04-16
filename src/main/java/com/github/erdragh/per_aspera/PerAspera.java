3a32162b44f8b31faf2462cd5ad80bd8e567ea10
Jan Bayer <enderjanyt@gmail.com>
2023-04-16 17:28:39 +0200

    move jet suit messages to action bar

--- src/main/java/com/github/erdragh/per_aspera/PerAspera.java
+++ src/main/java/com/github/erdragh/per_aspera/PerAspera.java
@@ -1,20 +1,14 @@
 package com.github.erdragh.per_aspera;
 
-import com.github.alexnijjar.ad_astra.registry.ModBlocks;
-import com.github.erdragh.per_aspera.blocks.LegacyBluePillar;
 import com.github.erdragh.per_aspera.items.JetSuitFlashCard;
 import com.github.erdragh.per_aspera.networking.C2SPackets;
 import com.mojang.logging.LogUtils;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
 import net.minecraft.core.Registry;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.RotatedPillarBlock;
 import org.slf4j.Logger;
 
 public class PerAspera implements ModInitializer {
@@ -29,10 +23,6 @@ public class PerAspera implements ModInitializer {
 
     public static final Item JET_SUIT_FLASH_CARD = new JetSuitFlashCard();
 
-    public static final Block LEGACY_BLUE_PILLAR = new LegacyBluePillar(FabricBlockSettings.copy(Blocks.CUT_COPPER).destroyTime(.15f).lightLevel((state) -> {
-        return 15;
-    }));
-
 
     @Override
     public void onInitialize() {
@@ -40,8 +30,6 @@ public class PerAspera implements ModInitializer {
         Registry.register(Registry.PARTICLE_TYPE, new ResourceLocation(MODID, "colored_jet_exhaust"), COLORED_JET_EXHAUST);
         Registry.register(Registry.PARTICLE_TYPE, new ResourceLocation(MODID, "end_rod_jet_exhaust"), END_ROD_JET_EXHAUST);
         Registry.register(Registry.PARTICLE_TYPE, new ResourceLocation(MODID, "bubbles_jet_exhaust"), BUBBLES_JET_EXHAUST);
-
-        Registry.register(Registry.BLOCK, new ResourceLocation("ad_astra", "blue_iron_pillar"), LEGACY_BLUE_PILLAR);
 
         Registry.register(Registry.ITEM, new ResourceLocation(MODID, "jet_suit_flash_card"), JET_SUIT_FLASH_CARD);
         LOGGER.info("Per Aspera initialized.");
